35a273f7773bef833ef86783b2083ea359330210
Simon Gaubert <sim.gaubert.sg@gmail.com>
2021-04-08 23:30:16 +0200

    partie 1 sans test

---
+++ libft/libft00.c
@@ -0,0 +1,93 @@
+#include "libft.h"
+
+void *ft_memset(void *s, int c, size_t n)
+{
+	size_t i;
+	char *c_s;
+
+	c_s = (char*)s;
+	i = 0;
+	while (i < n)
+		c_s[i++] = c;
+	return (s);
+}
+
+void ft_bzero(void *s, size_t n)
+{
+	size_t i;
+	char *c_s;
+
+	c_s = (char*)s;
+	i = 0;
+	while (i < n)
+		c_s[i++] = 0;
+}
+
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+	char *c_dest;
+	char *c_src;
+
+	c_dest = (char*)dest;
+	c_src = (char*)src;
+	i = 0;
+	while (i < n)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	return (dest);
+}
+
+void *ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	*c_dest;
+	char	*c_src;
+
+	c_dest = (char*)dest;
+	c_src = (char*)src;
+	i = 0;
+	while (i < n && c_src[i] != c)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	if (c_src[i] == c)
+		return (dest + i + 1);
+	return (NULL);
+}
+
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	int		j;
+	char	*c_dest;
+	char	*c_src;
+
+	c_dest = (char*)dest;
+	c_src = (char*)src;
+	if (c_src < c_dest)
+	{
+		i = n - 1;
+		j = 0;
+		while (j++ >= 0)
+		{
+			c_dest[i] = c_src[i];
+			i--;
+		}
+	}
+	else if (c_src > c_dest)
+	{
+		i = 0;
+		while (i < n)
+		{
+			c_dest[i] = c_src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
---
+++ libft/libft01.c
@@ -0,0 +1,49 @@
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*c_s;
+
+	c_s = (char*)s;
+	i = 0;
+	while (i < n && c_s[i] != c)
+		i++;
+	if (c_s[i] == c)
+		return (c_s + i);
+	return (NULL);
+}
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	char	*c_s1;
+	char	*c_s2;
+
+	c_s1 = (char*)s1;
+	c_s2 = (char*)s2;
+	i = 0;
+	while (i < n && c_s1[i] == c_s2[i])
+		i++;
+	return (c_s1[i] - c_s2[i]);
+}
+
+size_t ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int ft_isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
